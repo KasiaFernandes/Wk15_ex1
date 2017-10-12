4a7f3c6e7d39303a56309486d47c94d238cb5ce4
Kasia Fernandes <fernandes.kasia@gmail.com>
2017-10-12 08:39:57 +0200

    second commit

--- script-compiled.js
+++ script-compiled.js
@@ -1,5 +1,7 @@
 'use strict';
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 var sayHello = function sayHello() {
   return alert('Hello world!');
 };
@@ -17,17 +19,69 @@ console.log(helloWorld);
 var multiply = function multiply() {
   var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
   var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
-  return console.log(a * b);
+  return a * b;
 };
+
+console.log(multiply(2, 3));
 
 // Exercise 15.1.3
 
-var average = function average() {
+function average() {
+  var total = 0;
+
   for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
     args[_key] = arguments[_key];
   }
 
-  return args.reduce((n, function (sum) {
-    return n + sum;
-  }) / arr.length);
-};
+  args.forEach(function (x) {
+    return total += x;
+  });
+  return total / args.length;
+}
+
+console.log(average(1, 2, 3, 4, 5));
+
+/*
+const average = (...args) => {
+  const sum = args.reduce((total, amount) => {
+    total += amount
+    return total + amount;
+  }, 0);
+  return sum / args.length;
+}
+
+const average = (...args) => args.reduce((total, amount) => total + amount, 0) / args.length;
+*/
+
+// Exercise 15.1.4
+
+var grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+var first = grades[0],
+    second = grades[1],
+    rest = grades.slice(2);
+
+
+function average() {
+  var total = 0;
+
+  for (var _len2 = arguments.length, args = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+    args[_key2] = arguments[_key2];
+  }
+
+  args.forEach(function (x) {
+    return total += x;
+  });
+  return total / args.length;
+}
+
+console.log(average.apply(undefined, [first, second].concat(_toConsumableArray(rest))));
+
+// Exercise 15.1.5
+
+var strangeData = [1, 4, 'Iwona', false, 'Nowak'];
+var firstname = strangeData[2],
+    lastname = strangeData[4];
+
+
+console.log(firstname, lastname);
+console.log(firstname + ' ' + lastname);
--- script.js
+++ script.js
@@ -11,24 +11,44 @@ console.log(helloWorld)
 
 // Exercise 15.1.2
 
-const multiply = (a = 1, b = 1) => console.log(a * b);
+const multiply = (a = 1, b = 1) => (a * b);
+
+console.log(multiply(2, 3));
 
 // Exercise 15.1.3
 
 function average(...args) {
   var total = 0;
-  Array.from(args).forEach(x => total += x)
-  return total/Array.from(args).length
+  args.forEach(x => total += x);
+  return total / args.length;
 }
 
 console.log(average(1, 2, 3, 4, 5))
 
 /*
-const average = (...args) => Array.from(args).reduce((total, amount) => {
+const average = (...args) => {
+  const sum = args.reduce((total, amount) => {
     total += amount
-    return total/ args.length
-  });
+    return total + amount;
+  }, 0);
+  return sum / args.length;
+}
+
+const average = (...args) => args.reduce((total, amount) => total + amount, 0) / args.length;
 */
+
+// Exercise 15.1.4
+
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1]
+const [first, second, ...rest] = grades
+
+function average(...args) {
+  var total = 0;
+  args.forEach(x => total += x);
+  return total/args.length;
+}
+
+console.log(average(first, second, ...rest));
 
 
 // Exercise 15.1.5
